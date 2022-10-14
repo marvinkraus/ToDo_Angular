cd243f946a4db59cc36042949d95e88afa7636f7
DamnTom <tom@4werners.de>
2022-10-14 08:13:14 +0200

    listelements

--- src/app/models/todo-list.model.ts
+++ src/app/models/todo-list.model.ts
@@ -1,19 +1,14 @@
-//class that reprents a single model
+import { ConnectConfig } from "rxjs";
 
 export class TodoListModel {
     private _id: string;
     private _name: string;
-  
 
     constructor(name:string, id:string)
     {
         this._name = name;
         this._id = id;
-    
     }
-
-
-
 
     get id(): string|undefined{
         return this.id;
@@ -22,6 +17,7 @@ export class TodoListModel {
     get name():string {
         return this._name;
     }
+
 
 
 }
